27dd1cdec9e640fafec164a76d9d49672bb33dd0
StencelMichal <michal.stencel28@gmail.com>
2022-04-25 21:46:23 +0200

    [main] dev flag

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from logging import debug
 import logging
 
-from client.interaction.InteractionManager import InteractionManager
 
-
-def test_init():
+def initialize_interaction():
+    from client.interaction.InteractionManager import InteractionManager
     interaction_manager = InteractionManager()
     interaction_manager.start()
 
 
 def main():
-    import client.read_write_test.Read
+    pass
 
 
 if __name__ == '__main__':
+    dev = True
     logging.basicConfig(level=logging.DEBUG)
     debug("Initializing...")
-    test_init()
-    # main()
+    if not dev:
+        initialize_interaction()
+    main()
